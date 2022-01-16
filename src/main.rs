8e797fd3ba7e2db0d48e0dda36cd42b306ae8d12
Mfon Eti-mfon <mfonetimfon@gmail.com>
2022-01-16 01:27:41 +0100

    Write the hello world web app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let app = axum::Router::new().route("/", axum::routing::get(|| async { "Hello Axum!" }));
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
